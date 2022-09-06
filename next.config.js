7672a41508a303bd0396b8e9481a6ac0e92329ea
juankcba <juangblade@hotmail.com>
2022-09-06 19:21:14 -0300

    cambios

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["dercocenter-api.s3.us-east-1.amazonaws.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
