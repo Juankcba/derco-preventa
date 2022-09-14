c4eb47934275c0678dd40d0f62d2a7198a262f28
juankcba <juangblade@hotmail.com>
2022-09-14 16:07:54 -0300

    delete auth from app

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -10,7 +10,7 @@ import {
   Loading,
   Row,
 } from "@nextui-org/react";
-import { AuthContext } from "../../context";
+
 import NextLink from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -18,7 +18,7 @@ import { SearchIcon } from "./SearchIcon";
 const NavBar = () => {
   const [searchTerms, setSearchTerms] = useState("");
   const [loading, setLoading] = useState(false);
-  const { user, isLoggedIn, logout } = useContext(AuthContext);
+
   const router = useRouter();
   const navigateTo = (url: string) => {
     router.push(url);
@@ -96,66 +96,6 @@ const NavBar = () => {
             {loading && <Loading size="xs" css={{ paddingLeft: "20px" }} />}
           </Row>
         </Navbar.Item>
-        {!isLoggedIn && (
-          <Navbar.Item>
-            <Button
-              auto
-              flat
-              onClick={() => navigateTo(`/auth/login?p=${router.asPath}`)}
-            >
-              Ingresar
-            </Button>
-          </Navbar.Item>
-        )}
-        {isLoggedIn && (
-          <Dropdown placement="bottom-right">
-            <Navbar.Item>
-              <Dropdown.Trigger>
-                <Avatar
-                  bordered
-                  as="button"
-                  color="secondary"
-                  size="md"
-                  src={user?.image ? user.image : "/assets/img/avatar.jpg"}
-                />
-              </Dropdown.Trigger>
-            </Navbar.Item>
-
-            <Dropdown.Menu
-              aria-label="User menu actions"
-              color="secondary"
-              onAction={(actionKey: any) =>
-                actionKey != "logout" && router.push(actionKey)
-              }
-            >
-              <Dropdown.Item key="/" css={{ height: "$18" }} color={"primary"}>
-                <Text b color="inherit" css={{ d: "flex" }}>
-                  Hola
-                </Text>
-                <Text b color="inherit" css={{ d: "flex" }}>
-                  {user?.name}
-                </Text>
-              </Dropdown.Item>
-
-              <Dropdown.Item key="/profile" withDivider>
-                <NextLink href="/profile">
-                  <Link>Mi Perfil</Link>
-                </NextLink>
-              </Dropdown.Item>
-
-              <Dropdown.Item
-                key="logout"
-                withDivider
-                color="error"
-                css={{ height: "$15" }}
-              >
-                <Button auto light color="error" flat onClick={() => logout()}>
-                  Log Out
-                </Button>
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        )}
       </Navbar.Content>
       <Navbar.Toggle showIn="xs" />
     </Navbar>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,20 @@
 import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
+
 import "../styles/globals.scss";
 import { NextUIProvider } from "@nextui-org/react";
 import { ligthTheme } from "../themes";
-import { AuthProvider } from "../context";
+
 import { UiProvider } from "../context/ui";
 import { FilterProvider } from "../context/filters";
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <AuthProvider>
-        <FilterProvider>
-          <UiProvider>
-            <NextUIProvider theme={ligthTheme}>
-              <Component {...pageProps} />
-            </NextUIProvider>
-          </UiProvider>
-        </FilterProvider>
-      </AuthProvider>
-    </SessionProvider>
+    <FilterProvider>
+      <UiProvider>
+        <NextUIProvider theme={ligthTheme}>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </UiProvider>
+    </FilterProvider>
   );
 }
 
