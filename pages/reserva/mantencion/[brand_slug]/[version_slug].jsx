552cac844f867a8eb77a04272eece37fad48bf9f
Diego Vargas <dialvaro30@gmail.com>
2022-09-22 14:48:01 -0500

    add new page for mantencion

--- pages/reserva/mantencion/[brand_slug]/[version_slug].jsx
+++ pages/reserva/mantencion/[brand_slug]/[version_slug].jsx
@@ -1,98 +1,58 @@
-import React, { useState, useMemo, useEffect } from "react";
+import { PreventaLayout } from "../../../../components/Layouts"
 
-import { getVersionInfo, currency } from "../../../../utils";
-import { cmsApi, storeApi } from "../../../../apis";
-import {
-  Grid,
-  Text,
-  Link,
-  Row,
-  Card,
-  Input,
-  Spacer,
-  Button,
-} from "@nextui-org/react";
-
-import Image from "next/image";
-
-import NextLink from "next/link";
-
-import { PreventaLayout } from "../../../../components/Layouts/PreventaLayout";
-import { getVersionStoreInfo } from "./../../../../utils/getVersionStoreInfo";
-
-// interface Props {
-//   model: ModelResponse;
-// }
-
-const MantencionPage = ({ models }) => {
-  const [step, setStep] = useState(1);
-  const [user, setUser] = useState({
-    rut: "",
-    name: "",
-    lastname: "",
-    phone: "",
-    email: "",
-  });
-
-  const [model, setModel] = useState({});
-  useEffect(() => {
-    if (models.length > 0) {
-      setModel(models[0]);
-    }
-  }, [models]);
-
+const CarPage = () => {
   return (
-    <PreventaLayout
-      title={`${model.model_name} | DercoCenter `}
-      image={model.image_url}
-    >
-      <Text color="white">Preventa de Mantenciones</Text>
-    </PreventaLayout>
+    <PreventaLayout>
+      <div className="page_reserva_mantencion">
+        <div className="container">
+          <img src="https://s3.amazonaws.com/dercocenter.cl/cyber/backgorund-page-maintenance.jpg" alt="mobile" className="img_mobile" />
+          <div className="discount_mobile">
+            <span className="discount__number">35%</span>
+          </div>
+          <div className="content">
+            <div className="card">
+              <div className="discount">
+                <span className="discount__number">35%</span>
+              </div>
+              <div className="card__header">
+                <div className="card__header__title">
+                  <h3>Mantención Citycar 30.000km</h3>
+                  <h4>$270.000*</h4>
+                  <span className="card__header__title__discount">Antes <span>$370.000</span></span>
+                </div>
+                <button className="card__header__button">Pagar mantención</button>
+              </div>
+              <hr></hr>
+              <div className="card__body">
+                <h3 className="card__body__title">Si no estas seguro de que la mantención sea compatible con tu vehículo</h3>
+                <span className="card__body__text">Ingresa los siguientes datos:</span>
+                <div className="card__body__form">
+                  <select className="select_options">
+                    <option value="0" selected disabled>Selecciona la Marca</option>
+                    <option value="1">Marca 1</option>
+                    <option value="2">Marca 2</option>
+                  </select>
+                  <select className="select_options">
+                    <option value="0" selected disabled>Selecciona el Modelo</option>
+                    <option value="1">Modelo 1</option>
+                    <option value="2">Modelo 2</option>
+                  </select>
+                  <button className="button_verify">Verificar mantención</button>
+                </div>
+              </div>
+              <div className="card_footer">
+                <div className="card_footer__text">
+                  <span className="card_footer__text__title">Bases legales</span>
+                  <br />
+                  <span className="card_footer__text__subtitle">Hacer referencia a si mentiste o te equivocaste </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </PreventaLayout >
   );
 };
 
-export async function getStaticPaths() {
-  const {
-    data: { mantenciones },
-  } = await storeApi.get(
-    `/pre-order/cyber-dc/${process.env.NEXT_PUBLIC_PREVENTA}/cars`
-  );
-
-  const patchUrls = mantenciones.map((mantencion) => ({
-    version_slug: mantencion.version_slug,
-    brand_slug: mantencion.brand_slug,
-  }));
-
-  return {
-    paths: patchUrls.map((path) => ({
-      params: { ...path },
-    })),
-    fallback: false, // can also be true or 'blocking'
-  };
-}
-
-// `getStaticPaths` requires using `getStaticProps`
-export const getStaticProps = async ({ params }) => {
-  const { version_slug, brand_slug } = params;
-
-  const models = await getVersionStoreInfo(`MANCAMI10`);
-
-  console.log(models);
-
-  if (!models) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    // Passed to the page component as props
-    props: { models },
-    revalidate: 86400,
-  };
-};
-
-export default MantencionPage;
+export default CarPage;
